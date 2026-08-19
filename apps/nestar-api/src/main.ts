3408adc3cdac63289a7b6739456ed23d5c7031fd
uzalisher <uzalisher3@gmail.com>
2026-08-19 09:57:25 +0900

    feat: develop global GraphQL error handling system

--- apps/nestar-api/src/main.ts
+++ apps/nestar-api/src/main.ts
@@ -3,8 +3,9 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule); // Appmodel calling va integratsiyasi
+  const app = await NestFactory.create(AppModule); // Appmodel calling va integratsiyasi nestjs + express
   app.useGlobalPipes(new ValidationPipe()); // validation pipes global integratsiyasi
   await app.listen(process.env.PORT_API ?? 3000);
 }
 bootstrap();
+ 
