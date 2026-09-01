cdff194bc08371c33c95ab53f2ba3a2b74457b13
uzalisher <uzalisher3@gmail.com>
2026-09-01 23:43:32 +0900

    feat: develop createComment, updateComment and getComments GraphQL APIs

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -171,7 +171,7 @@ export class MemberService {
         
         const { _id, targetKey, modifier } = input; //@ts-ignore
         return await this.memberModel
-            .findOneAndUpdate(
+            .findByIdAndUpdate(
                 _id,
                 {
                     $inc: { [targetKey]: modifier },
